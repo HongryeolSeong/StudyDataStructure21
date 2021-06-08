cfa08b796481d39ef6aef6e08231e959ebe5fb21
ghd0276 <ghd0276@naver.com>
2021-06-08 17:28:01 +0900

    6/8 ox문제

--- 04Chapter/test.c
+++ 04Chapter/test.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-	int A, B;
-	while (scanf("%d %d", &A, &B) != EOF)
-		printf("%d\n", A + B);
+int main()
+{
+	int N;
+	int score;
+	char ox[85];
+	int s[100] = { 0 };
+
+	scanf("%d", &N);
+	getchar();
+
+	for (int i = 0; i < N; i++)
+	{
+		gets(ox);
+
+		score = 0;
+
+		for (int j = 0; j < strlen(ox); j++)
+		{
+			if (ox[j] == 'O')
+			{
+				score++;
+				s[j] += score;
+			}
+			else if (ox[j] == 'X')
+			{
+				score = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d\n", s[i]);
+	}
+
 	return 0;
 }
